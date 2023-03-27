6c6d39b70448e562134a68059d4f75196d95dc20
AtnRic <antoine.richard@student.junia.com>
2023-03-27 22:11:50 +0200

    Update calculatrice.js

--- calculatrice.js
+++ calculatrice.js
@@ -1,34 +1,3 @@
-/*
-console.log("hello");
-var calc = document.querySelector('.calculation');                         // la div d'affichage du calcul
-var result1 = document.querySelector('.result');                           // la div de d'affichage de résultat
-
-
-function addToInput(charClicked) {
-  calc.textContent += charClicked;
-}
-
-function clearInput() {
-  calc.textContent = "";
-}
-function clearResult() {
-  result1.textContent = "";
-}
-
-function computeResult() {
-  var input = calc.textContent;
-  try {
-    var result = eval(input);
-    result1.textContent = result;                               // on affiche le résultat
-    calc.textContent = "";                                      // on clear l'affichage de l'opération
-  } catch {
-    console.error("erreur dans l'opération");                // si l'opération n'est pas valide, on affiche "ERROR"
-    result1.textContent = "ERROR";
-  }
-  clearInput();
-}
-*/
-
 class BaseCalculator {
   constructor() {
     const lastCalculation = [];
